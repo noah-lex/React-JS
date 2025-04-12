ed02e3ea47bf6e13bd0d264cbf772859345215a5
Your Noah <noah.lexm@gmail.com>
2025-04-12 19:02:06 -0300

    array carrito y usuario

--- JS/app.js
+++ JS/app.js
@@ -3,6 +3,11 @@ const tortaSelvaNegra = {
     precio: '42000',
 }
 
+const Rogel = {
+    nombre: 'Torta Rogel',
+    precio: '36000',
+}
+
 class PerfilUsuario {
     constructor (nombre = ' ', mail = ' ', telefono = 0, contraseña = ' ', confirmarcontraseña = ' '){
         this.nombre = nombre;
@@ -13,8 +18,62 @@ class PerfilUsuario {
     }
 }
 
+const registroForm = document.getElementById("registroForm");
+
+registroForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const nombre = document.getElementById("nombre").value;
+  const mail = document.getElementById("mail").value;
+  const telefono = document.getElementById("telefono").value;
+  const contraseña = document.getElementById("contraseña").value;
+  const confirmar = document.getElementById("confirmarContraseña").value;
+
+  if (contraseña !== confirmar) {
+    alert("Las contraseñas no coinciden");
+    return;
+  }
+
+  const nuevoUsuario = new PerfilUsuario(nombre, mail, telefono, contraseña, confirmar);
+  console.log("Usuario creado:", nuevoUsuario);
+
 const user = new PerfilUsuario ('Mar', 'max18@gmail.com', 1124293536, 'contraseña123', 'contraseña123');
 const user2 = new PerfilUsuario ('Lea', 'leamr2@gmail.com', 1135369596, 'contra20', 'contra20');
 const user3 = new PerfilUsuario ('Alex', 'alexrizo84@gmail.com', 1128589090, 'frodo0208', 'frodo0208');
 
-console.log (user, user2, user3);
+localStorage.setItem("usuario", JSON.stringify(nuevoUsuario));
+})
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+const btnAgregar = document.getElementById("agregarTorta");
+const btnAgregarRogel = document.getElementById("agregarRogel");
+const divCarrito = document.getElementById("carrito");
+
+btnAgregar.addEventListener("click", () => {
+  carrito.push(tortaSelvaNegra);
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  mostrarCarrito();
+});
+
+btnAgregarRogel.addEventListener("click", () => {
+    carrito.push(Rogel);
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    mostrarCarrito();
+  });
+
+function mostrarCarrito() {
+    divCarrito.innerHTML = "<h2>Carrito</h2>";
+    carrito.forEach((producto, index) => {
+      divCarrito.innerHTML += `<p>${producto.nombre} - $${producto.precio} 
+      <button onclick="eliminarProducto(${index})">Eliminar</button></p>`;
+    });
+  }
+
+  function eliminarProducto(index) {
+    carrito.splice(index, 1);
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    mostrarCarrito();
+  }
+
+  mostrarCarrito();
